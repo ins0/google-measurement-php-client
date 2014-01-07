fecfb3857c8721140f053b0571df878a612167f8
ins0 <marco.rieger@aubi-plus.de>
2014-01-07 22:02:59 +0100

    user timing tracking added

---
+++ test/Racecore/GATracking/Tracking/User/TimingTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace Racecore\GATracking\Tracking\User;
+
+/**
+ * Class User Timing Test
+ *
+ * @author      Marco Rieger
+ * @package     Racecore\GATracking\Tracking\User
+ */
+class TimingTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var Timing
+     */
+    private $timing;
+
+    public function setUp()
+    {
+        $this->timing  = new Timing();
+    }
+
+    public function testPaketEqualsSpecification()
+    {
+        $timing = $this->timing;
+
+        $timing->setTimingCategory('Timing Category');
+        $timing->setTimingVariable('Timing Variable');
+        $timing->setTimingTime(1);
+        $timing->setTimingLabel('Timing Label');
+        $timing->setBrowserDnsLoadTime(2);
+        $timing->setBrowserPageDownloadTime(3);
+        $timing->setBrowserRedirectTime(4);
+        $timing->setBrowserTcpConnectTime(5);
+        $timing->setBrowserServerResponseTime(6);
+
+        $packet = $timing->getPaket();
+
+        $this->assertSame(
+            array(
+                't' => 'timing',
+                'utc' => 'Timing Category',
+                'utv' => 'Timing Variable',
+                'utt' => 1,
+                'utl' => 'Timing Label',
+                'dns' => 2,
+                'pdt' => 3,
+                'rrt' => 4,
+                'tcp' => 5,
+                'srt' => 6
+            ),
+            $packet
+        );
+    }
+
+}
+ 
