146a8122616eee183fa25131ac391aa3e4c0be20
ins0 <marco.rieger@aubi-plus.de>
2014-01-13 19:33:20 +0100

    cs in regex

--- src/Racecore/GATracking/GATracking.php
+++ src/Racecore/GATracking/GATracking.php
@@ -180,7 +180,6 @@ class GATracking
      */
     private function checkUUID( $uuid )
     {
-        $uuid = strtoupper( $uuid );
         return preg_match('#^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$#i', $uuid );
     }
 
