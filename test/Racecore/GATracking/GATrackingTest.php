429344e0c935640114fd758a8184255e439d40b4
Marco Rieger <marco.rieger@aubi-plus.de>
2014-12-21 18:27:13 +0100

    added tests

--- test/Racecore/GATracking/GATrackingTest.php
+++ test/Racecore/GATracking/GATrackingTest.php
@@ -2,6 +2,9 @@
 
 namespace Racecore\GATracking;
 
+use Racecore\GATracking\Request\TrackingRequest;
+use Racecore\GATracking\Request\TrackingRequestCollection;
+
 /**
  * Class GATrackingTest
  *
@@ -16,18 +19,113 @@ class GATrackingTest extends AbstractGATrackingTest
 
     public function setUp()
     {
-        $clientMock = $this->getClientMock();
-        $this->instance = new GATracking('foo', null, $clientMock);
+        $this->instance = new GATracking('fooId');
     }
 
     public function getClientMock()
     {
-        $clientMock = $this->getMock('GATracking\Client\CurlClient');
-        return $clientMock;
+        $class = $this->getMock('Racecore\GATracking\Client\Adapter\Socket');
+        return $class;
     }
 
     public function testInstanceIsGenerated()
     {
         $this->assertInstanceOf('Racecore\GATracking\GATracking', $this->instance);
+    }
+
+    public function testAnalyticsAccountIdIsSet()
+    {
+        $this->assertSame('fooId', $this->instance->getAnalyticsAccountUid());
+    }
+
+    public function testAnalyticsAccountIdCanBeSet()
+    {
+        $this->instance->setAnalyticsAccountUid('newFoo');
+        $this->assertSame('newFoo', $this->instance->getAnalyticsAccountUid());
+    }
+
+    public function testSetUpClientAdapterFromConstructor()
+    {
+        $this->assertInstanceOf(
+            'Racecore\GATracking\Client\AbstractClientAdapter',
+            $this->instance->getClientAdapter()
+        );
+    }
+
+    public function testDefaultClientAdapterIsSet()
+    {
+        $instance = new GATracking('foo');
+        $this->assertInstanceOf('Racecore\GATracking\Client\Adapter\Socket', $instance->getClientAdapter());
+    }
+
+    public function testOptionsCanSetOverConstructor()
+    {
+        $options = array(
+            'foo' => 'bar',
+            'baz' => array(
+                'foobaz'
+            )
+        );
+        $instance = new GATracking('foo', $options);
+
+        $this->assertSame($options, $instance->getOptions());
+    }
+
+    public function testOptionsCanSetOverMethod()
+    {
+        $options = array(
+            'foo' => 'bar',
+            'baz' => array(
+                'foobaz'
+            )
+        );
+        $this->instance->setOptions($options);
+
+        $this->assertSame($options, $this->instance->getOptions());
+    }
+
+    public function testOptionCanReveivedOverMethod()
+    {
+        $options = array(
+            'foo' => 'bar'
+        );
+        $this->instance->setOptions($options);
+
+        $this->assertSame('bar', $this->instance->getOption('foo'));
+    }
+
+    public function testSingleTracking()
+    {
+        $instance = $this->instance;
+
+        $trackingRequest = new TrackingRequest();
+        $trackingRequest->setPayload(array('foo' => 'bar'));
+        $trackingRequest->setResponseHeader(array('baz' => 'fooBar'));
+
+        $collection = new TrackingRequestCollection();
+        $collection->add($trackingRequest);
+
+        $clientMock = $this->getClientMock();
+        $clientMock->expects($this->once())
+            ->method('send')
+            ->will($this->returnValue($collection));
+
+        $tracking = $this->getMock('Racecore\GATracking\Tracking\Event');
+        $tracking->expects($this->once())
+            ->method('getPackage')
+            ->will($this->returnValue(array('test' => 'bar')));
+
+        $instance->setClientAdapter($clientMock);
+        $response = $instance->sendTracking($tracking);
+
+        $responsePayload = $response->getPayload();
+        $responseHeader = $response->getResponseHeader();
+
+        $this->assertInstanceOf('Racecore\GATracking\Request\TrackingRequest', $response);
+        $this->assertArrayHasKey('foo', $responsePayload);
+        $this->assertSame('bar', $responsePayload['foo']);
+
+        $this->assertArrayHasKey('baz', $responseHeader);
+        $this->assertSame('fooBar', $responseHeader['baz']);
     }
 }
