8c0310a3ec8721a4d6fb23d3706555ff5cd784b4
Marco Rieger <marco.rieger@aubi-plus.de>
2015-11-16 14:24:19 +0100

    added tests

---
+++ test/Racecore/GATracking/Tracking/TrackingTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Racecore\GATracking\Tracking;
+
+use Racecore\GATracking\AbstractGATrackingTest;
+
+/**
+ * Class TrackingTest
+ *
+ * @package       Racecore\GATracking\Tracking
+ */
+class TrackingTest extends AbstractGATrackingTest
+{
+    public function testCanSetTrackingProcessingTime()
+    {
+        $timeProcessed = new \DateTime();
+        $timeProcessed->modify('-1 hour');
+
+        $event = new Event();
+        $event->setEventCategory('foo');
+        $event->setEventAction('bar');
+        $event->setProcessingTime($timeProcessed->getTimestamp());
+
+        $package = $event->getPackage();
+        $this->assertEquals(3600000, $package['qt']);
+    }
+
+    public function testTrackingProcessingTimeIsNotSetWhenInvalid()
+    {
+        $timeProcessed = new \DateTime();
+        $timeProcessed->modify('+1 hour');
+
+        $event = new Event();
+        $event->setEventCategory('foo');
+        $event->setEventAction('bar');
+        $event->setProcessingTime($timeProcessed->getTimestamp());
+
+        $package = $event->getPackage();
+        $this->assertArrayNotHasKey('qt', $package);
+    }
+
+    public function testTrackingProcessingTimeThrowsNoticeWhenOver4Hours()
+    {
+        $this->setExpectedException(\PHPUnit_Framework_Error_Notice::class, 'Queue Time greater then 4 hours. Values greater than four hours may lead to hits not being processed!');
+
+        $timeProcessed = new \DateTime();
+        $timeProcessed->modify('-4 hours');
+
+        $event = new Event();
+        $event->setEventCategory('foo');
+        $event->setEventAction('bar');
+        $event->setProcessingTime($timeProcessed->getTimestamp());
+
+        $package = $event->getPackage();
+        $this->assertEquals(0, $package['qt']);
+    }
+}
