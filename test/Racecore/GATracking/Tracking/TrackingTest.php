52a245541488812d993cc245f7c652deac4f8355
Marco Rieger <marco.rieger@aubi-plus.de>
2015-11-16 14:48:10 +0100

    reflect qt changes to test

--- test/Racecore/GATracking/Tracking/TrackingTest.php
+++ test/Racecore/GATracking/Tracking/TrackingTest.php
@@ -13,45 +13,14 @@ class TrackingTest extends AbstractGATrackingTest
 {
     public function testCanSetTrackingProcessingTime()
     {
-        $timeProcessed = new \DateTime();
-        $timeProcessed->modify('-1 hour');
-
+        $queueTime = 12345678;
+        
         $event = new Event();
         $event->setEventCategory('foo');
         $event->setEventAction('bar');
-        $event->setProcessingTime($timeProcessed->getTimestamp());
+        $event->setQueueTime($queueTime);
 
         $package = $event->getPackage();
-        $this->assertEquals(3600000, $package['qt']);
-    }
-
-    public function testTrackingProcessingTimeIsNotSetWhenInvalid()
-    {
-        $timeProcessed = new \DateTime();
-        $timeProcessed->modify('+1 hour');
-
-        $event = new Event();
-        $event->setEventCategory('foo');
-        $event->setEventAction('bar');
-        $event->setProcessingTime($timeProcessed->getTimestamp());
-
-        $package = $event->getPackage();
-        $this->assertArrayNotHasKey('qt', $package);
-    }
-
-    public function testTrackingProcessingTimeThrowsNoticeWhenOver4Hours()
-    {
-        $this->setExpectedException('PHPUnit_Framework_Error_Notice', 'Queue Time greater than 4 hours. Values greater than four hours may lead to hits not being processed!');
-
-        $timeProcessed = new \DateTime();
-        $timeProcessed->modify('-4 hours');
-
-        $event = new Event();
-        $event->setEventCategory('foo');
-        $event->setEventAction('bar');
-        $event->setProcessingTime($timeProcessed->getTimestamp());
-
-        $package = $event->getPackage();
-        $this->assertEquals(0, $package['qt']);
+        $this->assertEquals($queueTime, $package['qt']);
     }
 }
